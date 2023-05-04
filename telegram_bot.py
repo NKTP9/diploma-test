1f08d73376bf418fc80c18ff90cdfdd8c57107a5
Nikita Gusev <c7520456@gmail.com>
2023-05-05 00:28:38 +0300

    commit

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,6 +2,7 @@ import telebot
 import model
 from docx import Document as DocxDocument
 from io import BytesIO
+import time
 
 TOKEN = "6289925879:AAHUUPHLxu8WV36nUALlvUREvHnf3597kgE"  # Замените на ваш токен
 
@@ -21,6 +22,7 @@ def process_message(message):
 
 @bot.message_handler(content_types=['document'])
 def process_document(message):
+    start_time = time.perf_counter()
     bot.reply_to(message, "Мои мыслительные процессы начали обрабатывать ваше резюме... Это может занять время, ожидайте!")
     if message.document.mime_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
         file_info = bot.get_file(message.document.file_id)
@@ -37,7 +39,10 @@ def process_document(message):
             text = "\n".join(full_text)
 
         bot_response = model.process_text(text)
+        end_time = time.perf_counter()
         bot.reply_to(message, bot_response)
+        elapsed_time = end_time - start_time
+        print(f"Время выполнения метода: {elapsed_time:0.6f} секунд")
     else:
         bot.reply_to(message, "Я пока понимаю и принимаю ваше резюме в формате DOCX. Пришлите мне его скорее!")
 
